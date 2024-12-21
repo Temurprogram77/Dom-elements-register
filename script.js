d1fd0294588838948d09c5f3a461617666a14c0f
Temur Coder <temurnarzullayev810@gmail.com>
2024-12-21 21:37:10 +0300

    add

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ let email = document.querySelector(".email");
 let password = document.querySelector(".password");
 let passwordd = document.querySelector(".passwordd");
 let btn = document.querySelector(".btn");
-let inp = document.querySelector("input");
+let inp1 = document.querySelector(".inp1");
+let inp2 = document.querySelector(".inp2");
+let inp3 = document.querySelector(".inp3");
+let inp4 = document.querySelector(".inp4");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  const emailRegEx = /[a-z0-9]{6,12}@gmail/;
-  const passwordRegEx = /^[0-9]{8,12}$/;
-  const nameRegEx = /[a-zA-Z]/;
+  let emailRegEx = /[a-z0-9]{6,12}@gmail/;
+  let passwordRegEx = /^[0-9]{8,12}$/;
+  let nameRegEx = /[a-zA-Z]/;
   let emailValue = email.value;
   let passwordValue = password.value;
-  let userName = username.value;
-  if (
-    username.value == "" ||
-    email.value == "" ||
-    password.value == "" ||
-    passwordd.value == ""
-  ) {
-    inp.style.border = '2px solid red'
+  let passworddValue = passwordd.value;
+  let nameValue = username.value;
+
+  if (emailValue == "" || passwordValue == "" || nameValue == "") {
+    alert("Avval to'ldiring");
   } else {
     if (
       emailRegEx.test(emailValue) &&
       passwordRegEx.test(passwordValue) &&
-      passwordd.value == password.value
+      passworddValue.value == password.value
     ) {
       alert("Malumotlar qabul qilindi😊. Malumot uchun rahmat.👌");
       userName.value = "";
